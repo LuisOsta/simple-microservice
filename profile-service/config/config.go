e648f32a85bd53bb549842678924ac082027cdb9
Luis Osta <luisosta1999@gmail.com>
2022-01-29 17:50:55 -0500

    added request signature check

--- profile-service/config/config.go
+++ profile-service/config/config.go
@@ -3,13 +3,15 @@ package config
 import "os"
 
 type Configuration struct {
-	MONGO_URI string
+	MONGO_URI   string
+	ROUTER_HOST string
 }
 
 func GetConfiguration() Configuration {
 
 	return Configuration{
-		MONGO_URI: os.Getenv("MONGO_URI"),
+		MONGO_URI:   os.Getenv("MONGO_URI"),
+		ROUTER_HOST: os.Getenv("ROUTER_HOST"),
 	}
 
 }
--- profile-service/router/router.go
+++ profile-service/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"github.com/user-service/entrypoint"
 	"github.com/user-service/profile"
 
 	"github.com/gin-gonic/gin"
@@ -9,6 +10,8 @@ import (
 func ConfigureRouter() *gin.Engine {
 	router := gin.Default()
 	router.SetTrustedProxies(nil)
+
+	router.Use(entrypoint.CheckRequestSignature)
 
 	router.POST("/", profile.HandleCreateProfile)
 
