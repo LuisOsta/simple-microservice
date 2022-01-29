0166682fad2346095d0fd431a7d320572a82459d
Luis Osta <luisosta1999@gmail.com>
2022-01-29 17:51:23 -0500

    cleaned up CheckRequestSignature

--- profile-service/entrypoint/entrypoint.go
+++ profile-service/entrypoint/entrypoint.go
@@ -10,12 +10,12 @@ import (
 // A Gin middleware that verifies the request signature to confirm it came from the router
 // In production and advanced use case, more sophisticated verification is needed.
 func CheckRequestSignature(c *gin.Context) {
-
-	log.Println("Checking request signature, " + c.Request.Host)
 	if c.Request.Host != config.GetConfiguration().ROUTER_HOST {
 		log.Println("Request originated from unauthorized host " + c.Request.Host)
 		c.JSON(400, gin.H{"error": "invalid request"})
 		c.Abort()
+	} else {
+		c.Request.Header.Set("Content-Type", "application/json; charset=utf-8")
+
 	}
-	c.Next()
 }
